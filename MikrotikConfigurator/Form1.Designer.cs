c7f110cdc7c5d67eef7d474774b96cca5341f9a5
potatoprince <martins.berzins16@gmail.com>
2018-06-22 12:25:13 -0700

    Added list of available routers and some methods

--- MikrotikConfigurator/Form1.Designer.cs
+++ MikrotikConfigurator/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,10 +42,11 @@
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 17.37968F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 82.62032F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 52F));
             this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -79,12 +82,34 @@
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(612, 380);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(133, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -100,6 +125,8 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- MikrotikConfigurator/Form1.cs
+++ MikrotikConfigurator/Form1.cs
@@ -12,29 +12,83 @@ namespace MikrotikConfigurator
 {
     public partial class Form1 : Form
     {
+        string availableRouters = "";
+
         public Form1()
         {
-            List<string> _routers = new List<string>(); //router list
-
             InitializeComponent();
 
-            // Add all the routers you've got
-            _routers.Add("RBM33G");
-            _routers.Add("RB3011UiAS");
-            _routers.Add("RB2011UiAS");
-            _routers.Add("RB493AH");
-            _routers.Add("LtAP-2HnD");
-            _routers.Add("CRS305-1G-4S+");
-            _routers.Add("CCR1036-12G-4S");
-            _routers.Add("CCR1036-8G-2S+");
-            _routers.Add("CCR1016-12G");
-            _routers.Add("CCR1009-8G-1S-1S+");
-            _routers.Add("CCR1009-7G-1C-1S+");
+            string[] _division = new string[] { "Endurance", "RMA" };
+            comboBox1.DataSource = _division;
+            comboBox1.SelectedIndex = 0;
 
-            // Make listbox datasource your routers
-            listBox1.DataSource = _routers;
+            
+
         }
 
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //router list
+            List<string> _routersEndurance = new List<string>();
+            List<string> _routersRMA = new List<string>();
+  
+            
 
+
+
+            if (comboBox1.SelectedItem.ToString().Equals("RMA"))
+            {
+                _routersRMA.Add("CCR1009-7G-1C-1S+");
+                _routersRMA.Add("CCR1099-8G-1S-1S+");
+                _routersRMA.Add("CCR1016-12G");
+                _routersRMA.Add("CCR1036-8G-2S+");
+                _routersRMA.Add("RB2011UiAS");
+                _routersRMA.Add("RB3011UiAS");
+
+                listBox1.DataSource = _routersRMA;
+                availableRouters = string.Join(" ", _routersRMA.ToArray());
+            }
+
+            if (comboBox1.SelectedItem.ToString().Equals("Endurance"))
+            {
+                _routersEndurance.Add("Audience");
+                _routersEndurance.Add("CCR1036-12G-4S");
+                _routersEndurance.Add("CRS305-1G-4S+");
+                _routersEndurance.Add("hAPac^2");
+                _routersEndurance.Add("RB760iGS (hEX s)");
+                _routersEndurance.Add("RBcAPGi-5acD2ND");
+                _routersEndurance.Add("RB_LtAP-2HnD");
+                _routersEndurance.Add("RBM33G");
+                _routersEndurance.Add("RBSXTsqG-5acD");
+                _routersEndurance.Add("RB493AH");
+
+                listBox1.DataSource = _routersEndurance;
+                availableRouters = string.Join(" ", _routersEndurance.ToArray());
+            }
+
+            
+           
+         
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //if (listBox1.DataSource.ToString().Contains(listBox1.SelectedItem.ToString()))
+            //{
+            //    textBox1.Text = "contains";
+            //}
+            textBox1.Text = availableRouters;
+            //textBox1.Text = listBox1.SelectedItem.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+       
     }
 }
